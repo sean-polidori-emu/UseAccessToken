1c7f0328a4d70ca36f44ca6ace2e8b3907881fa4
seanpolid <seanpolidori0@gmail.com>
2023-10-02 18:57:27 -0400

    A conflicting change to the second line

--- src/dummy/HelloWord.java
+++ src/dummy/HelloWord.java
@@ -4,7 +4,7 @@ public class HelloWord {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
-		System.out.println("Alternate");
+		System.out.println("Alternate 3");
 	}
 	
 }
