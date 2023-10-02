a3ad50949ca79ea17f7e8c77e0e183228f3f73d4
seanpolid <seanpolidori0@gmail.com>
2023-10-02 18:51:48 -0400

    Changed second print to alternate

--- src/dummy/HelloWord.java
+++ src/dummy/HelloWord.java
@@ -4,7 +4,7 @@ public class HelloWord {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
-		System.out.println("Another print statement");
+		System.out.println("Alternate");
 	}
 	
 }
